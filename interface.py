20d39a60f5ff89ff2779846776aa571ce3e6b43a
veetiengblom <Veeti_engblom@hotmail.com>
2023-03-09 18:35:44 +0200

    modified python file
    added comments, fixed insert character, fixed update character,

--- interface.py
+++ interface.py
@@ -5,6 +5,7 @@ cur = db.cursor()
 
 def main():
     userInput = -1
+    #Menu
     while(userInput != "0"):
         print("\nMenu options:")
         print("1: Add character")
@@ -18,7 +19,7 @@ def main():
             print("Invalid input")
             continue
         if userInput == "1":
-            insertPlayer()
+            insertCharacter()
         if userInput == "2":
             updateCharacter()
         if userInput == "3":
@@ -33,50 +34,72 @@ def main():
     db.close()        
     return
 
-def insertPlayer():
+#Add character to database
+def insertCharacter():
     playerName = input("Add characher name: ")
+    #Check if name is empty
     if playerName == "":
         print("Invalid name")
         return
     selectedWeapon = int(input("Select weapon for you character: \n 1) Sword, 2) Axe, 3) Bow, 4), Spear, 5) Staff: "))
+    #Check if weapon choice is valid
     if selectedWeapon < 1 or selectedWeapon > 5:
         print("Invalid weapon selection")
         return
+    if selectedWeapon == 1:
+        weaponName = "Sword"
+    elif selectedWeapon == 2:
+        weaponName = "Axe"
+    elif selectedWeapon == 3:
+        weaponName = "Bow"
+    elif selectedWeapon == 4:
+        weaponName = "Spear"
+    elif selectedWeapon == 5:
+        weaponName = "Staff"
     health = int(input("Add health (10 - 1 000): "))
+    #Check if health is valid
     if health < 10 or health > 1000:
         print("Invalid health selection")
         return
     try:
+        #Add character to database
         cur.execute("SELECT PlayerID FROM Character ORDER BY PlayerID DESC LIMIT 1;")
         playerID = cur.fetchone()[0]
         playerID = int(playerID) + 1
         cur.execute("INSERT INTO Character (PlayerID, name, health, isSelected) VALUES (?, ?, ?, ?);", (playerID, playerName, health, selectedWeapon))
+        #Add weapon to inventory
+        cur.execute("INSERT INTO Inventory (PlayerID, WeaponID) VALUES (?, ?);", (playerID, selectedWeapon))
         db.commit()
+    #Raise error if name already exists
     except sqlite3.IntegrityError:
         print("Name already exists. Choose different name.")
         db.rollback()
-        return      
+        return
+    #Raise error if database error      
     except db.Error:
         print("Error!")
         db.rollback()
         return
-    print("Character added: \n PlayerID:", playerID, " \n Name: ", playerName, " \n Weapon: ", selectedWeapon, " \n Health: ", health)
+    print("Character added: \n PlayerID:", playerID, " \n Name: ", playerName, " \n Weapon: ", weaponName, " \n Health: ", health)
     return
 
+#Update character in database
 def updateCharacter():
     characterID = input("What is the character's ID: ")
     try:
+        #Select character from database and print name and current weapon and health
         cur.execute("SELECT * FROM Character WHERE PlayerID = ?;", (characterID,))
         data = cur.fetchall()
         cur.execute("SELECT name FROM Weapon WHERE WeaponID = ?;", (data[0][3],))
         weaponSelected = cur.fetchone()[0]
+        print("Character selected: ", data[0][1])
+        print("Your character's current weapon is: ", weaponSelected)
+        print("Your character's current health is: ", data[0][2])
+        #Select all weapons from inventory and add them to a list if there are any
         cur.execute("SELECT WeaponID FROM Inventory WHERE PlayerID = ?;", (characterID,))
         weaponID = cur.fetchall()
         cur.execute("SELECT name FROM Weapon WHERE WeaponID = ?;", (weaponID[0][0],))
         weaponName = cur.fetchone()
-        print("Character selected: ", data[0][1])
-        print("Your character's current weapon is: ", weaponSelected)
-        print("Your character's current health is: ", data[0][2])
         cur.execute("SELECT WeaponID FROM Inventory WHERE PlayerID = ?;", (characterID,))
         weaponID = cur.fetchall()
         listOfWeapons = []
@@ -85,16 +108,19 @@ def updateCharacter():
             weaponName = cur.fetchone()[0]
             listOfWeapons.append(weaponName)
         print()
+        #If there are no weapons in inventory, print error message and ask for new health
         if len(listOfWeapons) == 0:
             print("Your inventory is empty. Cannot change weapon.")
             health = int(input("Add new health (10 - 1 000): "))
             if health < 10 or health > 1000:
                 print("Invalid health selection")
                 return
+            #Update health
             cur.execute("UPDATE Character SET health = ? WHERE PlayerID = ?;", (health, characterID,))
             db.commit()
             print("Character updated: \n New health: ", health)
             return
+        #If there are weapons in inventory, print them and ask for new weapon and health
         else:
             print("Select new weapon from your inventory:")
             for i in range(len(listOfWeapons)):
@@ -103,44 +129,64 @@ def updateCharacter():
             if selectedWeapon < 1 or selectedWeapon > len(listOfWeapons):
                 print("Invalid weapon selection")
                 return
+            weaponName = listOfWeapons[selectedWeapon-1]
+            if weaponName == "Sword":
+                weaponID = 1
+            elif weaponName == "Axe":
+                weaponID = 2
+            elif weaponName == "Bow":
+                weaponID = 3
+            elif weaponName == "Spear":
+                weaponID = 4
+            elif weaponName == "Staff":
+                weaponID = 5
             health = int(input("Add new health (10 - 1 000): "))
             if health < 10 or health > 1000:
                 print("Invalid health selection")
                 return
-            cur.execute("UPDATE Character SET isSelected = ?, health = ? WHERE PlayerID = ?;", (selectedWeapon, health, characterID,))
+            #Update weapon and health
+            cur.execute("UPDATE Character SET isSelected = ?, health = ? WHERE PlayerID = ?;", (weaponID, health, characterID,))
             db.commit()
-            cur.execute("SELECT name FROM Weapon WHERE WeaponID = ?;", (selectedWeapon,))
+            cur.execute("SELECT name FROM Weapon WHERE WeaponID = ?;", (weaponID,))
             newWeaponName = cur.fetchone()[0]
             print("Character updated: \n New weapon: ", newWeaponName, " \n New health: ", health)
             return
+    #Raise error if character does not exist
     except IndexError:
         print("Player with PlayerID '{}' does not exist".format(characterID))
         return
+    #Raise error if database error
     except db.Error:
         print("Error!")
         db.rollback()
         return
 
+#Delete character from database
 def deleteCharacter():
     characterID = input("What is the player's PlayerID you want to delete: ")
-    #try:
-    cur.execute("SELECT name FROM Character WHERE PlayerID = ?;", (characterID,))
-    name = cur.fetchone()[0]
-    cur.execute("DELETE FROM Character WHERE PlayerID = ?;", (characterID,))
-    db.commit()
-    # except TypeError:
-    #     print("Player with PlayerID '{}' does not exist".format(characterID))
-    #     return
-    # except db.Error:
-    #     print("Error!")
-    #     db.rollback()
-    #     return
+    try:
+        #Delete character from database
+        cur.execute("SELECT name FROM Character WHERE PlayerID = ?;", (characterID,))
+        name = cur.fetchone()[0]
+        cur.execute("DELETE FROM Character WHERE PlayerID = ?;", (characterID,))
+        db.commit()
+    #Raise error if character does not exist
+    except TypeError:
+        print("Player with PlayerID '{}' does not exist".format(characterID))
+        return
+    #Raise error if database error
+    except db.Error:
+        print("Error!")
+        db.rollback()
+        return
     print("Character '{}' deleted".format(name))
     return
 
+#Search for character's inventory
 def searchInventory():
     characterID = input("What PlayerID's inventory you want to search: ")
     try:    
+        #Select character and inventory from database and print them
         cur.execute("SELECT name FROM Character WHERE PlayerID = ?;", (characterID,))
         playerName = cur.fetchone()[0]
         cur.execute("SELECT WeaponID FROM Inventory WHERE PlayerID = ?;", (characterID,))
@@ -154,15 +200,18 @@ def searchInventory():
             cur.execute("SELECT name FROM Weapon WHERE WeaponID = ?;", (weaponID[i][0],))
             weaponName = cur.fetchone()[0]
             print(weaponName)
+    #Raise error if character does not exist
     except TypeError:
         print("Player with PlayerID '{}' does not exist".format(characterID))
         return
+    #Raise error if database error
     except db.Error:
         print("Error!")
         db.rollback()
         return
     return
 
+#Print example queries
 def printQueries():
     try:
         print("")
@@ -172,7 +221,6 @@ def printQueries():
         print("(PlayerID, name, health, isSelected)")
         print("----------------------------------")
         cur.execute("SELECT * FROM Character;")
-        
         data = cur.fetchall()
         for i in range(len(data)):
             print(data[i])
@@ -188,11 +236,11 @@ def printQueries():
             print(data[i])
         
         print("")
-        print("Player 1 weapons stats")
-        print("SELECT * FROM Weapon INNER JOIN Inventory ON Weapon.WeaponID = Inventory.WeaponID WHERE PlayerID = 1;")
+        print("All players weapons stats")
+        print("SELECT * FROM Weapon INNER JOIN Inventory ON Weapon.WeaponID = Inventory.WeaponID;")
         print("(WeaponID, damage, name, PlayerID, WeaponID)")
         print("----------------------------------")
-        cur.execute("SELECT * FROM Weapon INNER JOIN Inventory ON Weapon.WeaponID = Inventory.WeaponID WHERE PlayerID = 1;")
+        cur.execute("SELECT * FROM Weapon INNER JOIN Inventory ON Weapon.WeaponID = Inventory.WeaponID;")
         data = cur.fetchall()
         for i in range(len(data)):
             print(data[i])
@@ -208,11 +256,11 @@ def printQueries():
             print(data[i])
         
         print("")
-        print("All weapons and their attack types")
-        print("SELECT * FROM Weapon INNER JOIN AttackType ON Weapon.WeaponID = AttackType.WeaponID ORDER BY Weapon.name;")
+        print("All weapons and their attack types where attack multiplier is over 1")
+        print("SELECT * FROM Weapon INNER JOIN AttackType ON Weapon.WeaponID = AttackType.WeaponID WHERE AttackType.AttackMultiplier > 1 ORDER BY Weapon.name;")
         print("(WeaponID, damage, name, WeaponID, AttackMultiplier)")
         print("----------------------------------")
-        cur.execute("SELECT * FROM Weapon INNER JOIN AttackType ON Weapon.WeaponID = AttackType.WeaponID ORDER BY Weapon.name;")
+        cur.execute("SELECT * FROM Weapon INNER JOIN AttackType ON Weapon.WeaponID = AttackType.WeaponID WHERE AttackType.AttackMultiplier > 1 ORDER BY Weapon.name;")
         data = cur.fetchall()
         for i in range(len(data)):
             print(data[i])
--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
-
+/*
 CREATE TABLE Character (
     PlayerID int NOT NULL,
     name varchar(255) NOT NULL UNIQUE,
